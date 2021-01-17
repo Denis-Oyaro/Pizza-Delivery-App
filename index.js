10522686f5ff7c808d1a3a16a64107aecfc3038b
Oyaro, Dennis <Dennis.Oyaro@uhn.ca>
2021-01-16 21:35:44 -0500

    Add admin CLI

--- index.js
+++ index.js
@@ -1,147 +1,27 @@
 /*
- * Entry point of app
+ * Entry point of application
  */
 
 // Dependencies
-const http = require('http');
-const config = require('./lib/config');
-const handlers = require('./lib/handlers');
-const helpers = require('./lib/helpers');
-const { StringDecoder } = require('string_decoder');
+const server = require('./lib/server');
+const cli = require('./lib/cli');
 
-// Instantiate http server
-const httpServer = http.createServer((req, res) => {
-    // Get the url and parse
-    const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
+// Container of app
+const app = {};
 
-    // Get the path
-    const path = parsedUrl.pathname;
-    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+// Init function
+app.init = function () {
+    // Start server
+    server.init();
 
-    // Get the query string as an object
-    const queryStringObject = parsedUrl.searchParams;
-
-    // Get the http method
-    const method = req.method.toLowerCase();
-
-    // Get the headers as an object
-    const headers = req.headers;
-
-    // Get the payload, if any
-    const decoder = new StringDecoder('utf8');
-    let buffer = '';
-    req.on('data', function (data) {
-        buffer += decoder.write(data);
-    });
-    req.on('end', function () {
-        buffer += decoder.end();
-
-        // Choose the handler this request should go to. If one is not found, use the notFound handler
-        let chosenHandler = router[trimmedPath] || handlers.notFound;
-
-        // If the request is within the public directory, use the public handler instead
-        if (trimmedPath.includes('public/')) {
-            chosenHandler = handlers.public;
-        }
-
-        // Construct the data object to send to handler
-        const data = { trimmedPath, queryStringObject, method, headers, payload: helpers.parseJsonToObject(buffer) };
-
-        // Route the request to the chosen handler
-        chosenHandler(data, function (statusCode, payload, contentType) {
-            // Determine the type of response (fallback to JSON)
-            contentType = typeof contentType === 'string' ? contentType : 'json';
-
-            // Use the status code called back by the handler, or default to 200
-            statusCode = typeof statusCode === 'number' ? statusCode : 200;
-
-            // Return the response parts that are content specific
-            let payloadStr = '';
-            if (contentType === 'json') {
-                res.setHeader('Content-Type', 'application/json');
-
-                // Use the payload called back by the handler, or default to an empty object
-                payload = typeof payload === 'object' && payload !== null ? payload : {};
-
-                // Convert payload to string
-                payloadStr = JSON.stringify(payload);
-            }
-
-            if (contentType === 'html') {
-                res.setHeader('Content-Type', 'text/html');
-
-                // Use the payload called back by the handler, or default to an empty string
-                payloadStr = typeof payload === 'string' ? payload : '';
-            }
-
-            if (contentType === 'favicon') {
-                res.setHeader('Content-Type', 'image/x-icon');
-
-                // Use the payload called back by the handler, or default to an empty string
-                payloadStr = typeof payload !== 'undefined' ? payload : '';
-            }
-
-            if (contentType === 'css') {
-                res.setHeader('Content-Type', 'text/css');
-
-                // Use the payload called back by the handler, or default to an empty string
-                payloadStr = typeof payload !== 'undefined' ? payload : '';
-            }
-
-            if (contentType === 'png') {
-                res.setHeader('Content-Type', 'image/png');
-
-                // Use the payload called back by the handler, or default to an empty string
-                payloadStr = typeof payload !== 'undefined' ? payload : '';
-            }
-
-            if (contentType === 'jpg') {
-                res.setHeader('Content-Type', 'image/jpeg');
-
-                // Use the payload called back by the handler, or default to an empty string
-                payloadStr = typeof payload !== 'undefined' ? payload : '';
-            }
-
-            if (contentType === 'plain') {
-                res.setHeader('Content-Type', 'text/plain');
-
-                // Use the payload called back by the handler, or default to an empty string
-                payloadStr = typeof payload !== 'undefined' ? payload : '';
-            }
-
-            // Return the response parts that are common to all content types
-            res.writeHead(statusCode);
-            res.end(payloadStr);
-
-            // If response status code is 200, log green, otherwise, log red
-            if (statusCode === 200) {
-                console.log('\x1b[32m%s\x1b[0m', `${method.toUpperCase()} /${trimmedPath} ${statusCode}`);
-            } else {
-                console.log('\x1b[31m%s\x1b[0m', `${method.toUpperCase()} /${trimmedPath} ${statusCode}`);
-            }
-        });
-    });
-});
-
-// Start server
-httpServer.listen(config.port, () => {
-    console.log('\x1b[33m%s\x1b[0m', `listening on port ${config.port}`);
-});
-
-// Route requests
-router = {
-    '': handlers.index,
-    'account/create': handlers.accountCreate,
-    'account/edit': handlers.accountEdit,
-    'account/deleted': handlers.accountDeleted,
-    'session/create': handlers.sessionCreate,
-    'session/deleted': handlers.sessionDeleted,
-    'menuItemsAndShoppingCart/all': handlers.menuItemsAndShoppingCartList,
-    'order/create': handlers.orderCreate,
-    'order/success': handlers.orderSuccess,
-    'api/users': handlers.users,
-    'api/tokens': handlers.tokens,
-    'api/menuItems': handlers.menuItems,
-    'api/shoppingCart': handlers.shoppingCart,
-    'api/orders': handlers.orders,
+    // Start cli, but make sure it starts last
+    setTimeout(() => {
+        cli.init();
+    }, 50);
 };
+
+// Start app
+app.init();
+
+// Export module
+module.exports = app;
